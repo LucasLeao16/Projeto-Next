62213670f503bc73893d6eacf916bd6ef1b7ec23
LucasLeao16 <lucas99br@gmail.com>
2024-05-18 19:23:47 -0300

    Setup inicial do projeto

---
+++ src/app/games/layout.tsx
@@ -0,0 +1,12 @@
+export default function GamesLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div>
+      <h1>Pagina Games Layout</h1>
+      {children}
+    </div>
+  );
+}
---
+++ src/app/games/nintendo/page.tsx
@@ -0,0 +1,3 @@
+export default function Nintendo() {
+  return <h1>Pagina Nintendo</h1>;
+}
