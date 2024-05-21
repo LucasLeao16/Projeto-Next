8bc509146aa68bf888a15582ea28de624b9b4fcf
LucasLeao16 <lucas99br@gmail.com>
2024-05-21 14:08:36 -0300

    "Navbar refactor"

---
+++ src/app/components/Icons/Icons.tsx
@@ -0,0 +1,6 @@
+export { AwardIcon } from "./icons/Awardicon";
+export { GamepadIcon } from "./icons/Gamepadicon";
+export { HomeIcon } from "./icons/Homeicon";
+export { RouteIcon } from "./icons/Routeicon";
+export { SmileIcon } from "./icons/Smileicon";
+//https://www.iconfinder.com/search?q=&iconset=font-awesome-solid
--- src/app/components/Navbar/Navbar.tsx
+++ src/app/components/Navbar/Navbar.tsx
@@ -1,33 +1,68 @@
+import {
+  AwardIcon,
+  GamepadIcon,
+  HomeIcon,
+  RouteIcon,
+  SmileIcon,
+} from "../Icons/Icons";
+
+const NavbarList = ({ children, className }) => {
+  return (
+    <ul
+      className={`my-4 border-t border-indigo-400/20 hover:border-indigo-400/40 ${className}`}
+    >
+      {children}
+    </ul>
+  );
+};
+
+const NavbarListItem = ({ children, className }) => {
+  return (
+    <li
+      className={`my-2 flex cursor-pointer items-center gap-2 rounded bg-transparent p-2 hover:bg-indigo-400/40 hover:text-slate-100 ${className}`}
+    >
+      {children}
+    </li>
+  );
+};
+
 export const Navbar = () => {
   return (
-    <nav className="flex h-screen w-72 flex-col gap-4 border-r  border-indigo-400/40  bg-slate-900 p-2 hover:border-indigo-400/80">
-      <div>
+    <nav className="flex h-screen w-72 flex-col border-r  border-indigo-400/20  bg-slate-900 p-2 text-slate-300 hover:border-indigo-400/40">
+      <div className="flex items-center justify-center">
         <img
-          className="max-w-full p-2 "
+          className="h-12 w-auto p-2 "
           src="https://emersonbroga.com/e/assets/emersonbroga-logo-name-pink.png"
           alt="logo"
         />
       </div>
-      <ul className="my-4 flex-grow border-t  border-indigo-400/20 hover:border-indigo-400/40">
-        <li className=" my-2 cursor-pointer rounded bg-transparent p-2 hover:bg-slate-800">
+      <NavbarList className={"flex-grow"}>
+        <NavbarListItem>
+          <HomeIcon className="h-4 w-4" />
           Home
-        </li>
-        <li className=" my-2 cursor-pointer rounded bg-transparent p-2 hover:bg-slate-800">
-          Games
-        </li>
-        <li className=" my-2 cursor-pointer rounded bg-transparent p-2 hover:bg-slate-800">
-          Top 10
-        </li>
-        <li className=" my-2 cursor-pointer rounded bg-transparent p-2 hover:bg-slate-800">
-          Walktrought
-        </li>
-      </ul>
+        </NavbarListItem>
 
-      <ul className=" my-4 border-t border-indigo-400/20 hover:border-indigo-400/40">
-        <li className=" my-2 cursor-pointer rounded bg-transparent p-2 hover:bg-slate-800">
-          User
-        </li>
-      </ul>
+        <NavbarListItem>
+          <GamepadIcon className="h-4 w-4" />
+          Games
+        </NavbarListItem>
+
+        <NavbarListItem>
+          <AwardIcon className="h-4 w-4" />
+          Top 10
+        </NavbarListItem>
+
+        <NavbarListItem>
+          <RouteIcon className="h-4 w-4" />
+          Walktrought
+        </NavbarListItem>
+      </NavbarList>
+
+      <NavbarList>
+        <NavbarListItem>
+          <SmileIcon className="h-4 w-4" /> User
+        </NavbarListItem>
+      </NavbarList>
     </nav>
   );
 };
